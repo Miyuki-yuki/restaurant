147957ad9e70fbb2fcc9c74deb58461c5c15c865
Miyuki <87884079+miyuki-KK@users.noreply.github.com>
2023-09-19 12:08:17 +0900

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Miyuki Kikuchi on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
